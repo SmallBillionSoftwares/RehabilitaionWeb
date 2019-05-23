8fe09ac33697ad4ea55da731d244eaa44fb5a7a4
Daniel Didah <didahdaniel@gmail.com>
2019-05-23 17:49:18 +0300

    web design

---
+++ admin.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.nav-trigger').click(function() {
+		$('.side-nav').toggleClass('visible');
+		
+	});
+});
+function report(){
+									 document.getElementById("titleAdmin").innerHTML="Reported Drug Incidences";
+									
+									frame();
+								}
+								function frame(){
+									document.getElementById("frameDiv1").style.display="none";
+								var x = document.getElementById("frameDiv").style.display="block";
+								
+
+  								}
+  								function enrol(){
+  									document.getElementById("frameDiv").style.display="none";
+  										document.getElementById("frameDiv1").style.display="block";
+  								}
